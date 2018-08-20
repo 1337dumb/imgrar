5e5b52a0daf4e372d68b372301e2350872fbf717
sebastian <dead@hotmail.com>
2018-08-21 00:29:11 +0200

    added files
    gay assfasdfwc

---
+++ imgrar/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace imgrar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_image = new System.Windows.Forms.TextBox();
+            this.txt_archive = new System.Windows.Forms.TextBox();
+            this.btn_image = new System.Windows.Forms.Button();
+            this.btn_archive = new System.Windows.Forms.Button();
+            this.lbl_image = new System.Windows.Forms.Label();
+            this.lbl_archive = new System.Windows.Forms.Label();
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.btn_manipulate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_image
+            // 
+            this.txt_image.Location = new System.Drawing.Point(80, 37);
+            this.txt_image.Name = "txt_image";
+            this.txt_image.Size = new System.Drawing.Size(111, 20);
+            this.txt_image.TabIndex = 0;
+            this.txt_image.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_archive
+            // 
+            this.txt_archive.Location = new System.Drawing.Point(80, 63);
+            this.txt_archive.Name = "txt_archive";
+            this.txt_archive.Size = new System.Drawing.Size(111, 20);
+            this.txt_archive.TabIndex = 1;
+            this.txt_archive.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btn_image
+            // 
+            this.btn_image.Location = new System.Drawing.Point(197, 35);
+            this.btn_image.Name = "btn_image";
+            this.btn_image.Size = new System.Drawing.Size(75, 23);
+            this.btn_image.TabIndex = 2;
+            this.btn_image.Text = "open";
+            this.btn_image.UseVisualStyleBackColor = true;
+            this.btn_image.Click += new System.EventHandler(this.btn_image_Click);
+            // 
+            // btn_archive
+            // 
+            this.btn_archive.Location = new System.Drawing.Point(197, 61);
+            this.btn_archive.Name = "btn_archive";
+            this.btn_archive.Size = new System.Drawing.Size(75, 23);
+            this.btn_archive.TabIndex = 3;
+            this.btn_archive.Text = "open";
+            this.btn_archive.UseVisualStyleBackColor = true;
+            this.btn_archive.Click += new System.EventHandler(this.btn_archive_Click);
+            // 
+            // lbl_image
+            // 
+            this.lbl_image.AutoSize = true;
+            this.lbl_image.Location = new System.Drawing.Point(12, 40);
+            this.lbl_image.Name = "lbl_image";
+            this.lbl_image.Size = new System.Drawing.Size(51, 13);
+            this.lbl_image.TabIndex = 4;
+            this.lbl_image.Text = "image file";
+            // 
+            // lbl_archive
+            // 
+            this.lbl_archive.AutoSize = true;
+            this.lbl_archive.Location = new System.Drawing.Point(12, 66);
+            this.lbl_archive.Name = "lbl_archive";
+            this.lbl_archive.Size = new System.Drawing.Size(58, 13);
+            this.lbl_archive.TabIndex = 5;
+            this.lbl_archive.Text = "archive file";
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.lbl_title.Location = new System.Drawing.Point(12, 9);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(57, 20);
+            this.lbl_title.TabIndex = 6;
+            this.lbl_title.Text = "imgrar";
+            // 
+            // btn_manipulate
+            // 
+            this.btn_manipulate.Location = new System.Drawing.Point(12, 90);
+            this.btn_manipulate.Name = "btn_manipulate";
+            this.btn_manipulate.Size = new System.Drawing.Size(260, 23);
+            this.btn_manipulate.TabIndex = 7;
+            this.btn_manipulate.Text = "manipulate file";
+            this.btn_manipulate.UseVisualStyleBackColor = true;
+            this.btn_manipulate.Click += new System.EventHandler(this.btn_manipulate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 125);
+            this.Controls.Add(this.btn_manipulate);
+            this.Controls.Add(this.lbl_title);
+            this.Controls.Add(this.lbl_archive);
+            this.Controls.Add(this.lbl_image);
+            this.Controls.Add(this.btn_archive);
+            this.Controls.Add(this.btn_image);
+            this.Controls.Add(this.txt_archive);
+            this.Controls.Add(this.txt_image);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "imgrar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_image;
+        private System.Windows.Forms.TextBox txt_archive;
+        private System.Windows.Forms.Button btn_image;
+        private System.Windows.Forms.Button btn_archive;
+        private System.Windows.Forms.Label lbl_image;
+        private System.Windows.Forms.Label lbl_archive;
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.Button btn_manipulate;
+    }
+}
+
---
+++ imgrar/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace imgrar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string str_image_ext= ".jpg";
+        public static string str_archive_ext = ".rar";
+
+        private void btn_image_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd_image = new OpenFileDialog())
+            {
+                ofd_image.Title = "imgrar - open image file";
+                ofd_image.Filter = "image files|*.jpg;*.jpeg;*.png|*.jpg;*.jpeg;*.png|*.jpg;*.jpeg;*.png";
+                ofd_image.FilterIndex = 2;
+                ofd_image.RestoreDirectory = true;
+
+                if (ofd_image.ShowDialog() == DialogResult.OK)
+                {
+                    txt_image.Text = ofd_image.FileName;
+                    str_image_ext = Path.GetExtension(txt_image.Text);
+                }
+            }
+        }
+
+        private void btn_archive_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Notice: Password protecting the archive makes it more secure.", "imgrar");
+
+            using (OpenFileDialog ofd_archive = new OpenFileDialog())
+            {
+                ofd_archive.Title = "imgrar - open archive file";
+                ofd_archive.Filter = "archive files|*.zip;*.rar|*.zip;*.rar|*.zip;*.rar";
+                ofd_archive.FilterIndex = 2;
+                ofd_archive.RestoreDirectory = true;
+
+                if (ofd_archive.ShowDialog() == DialogResult.OK)
+                {
+                    txt_archive.Text = ofd_archive.FileName;
+                    str_archive_ext = Path.GetExtension(txt_archive.Text);
+                }
+            }
+        }
+
+        
+        public string str_random()
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
+            return new string(Enumerable.Repeat(chars, 10)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void btn_manipulate_Click(object sender, EventArgs e)
+        {
+            if (!(File.Exists(txt_image.Text))
+                ||!(File.Exists(txt_archive.Text))
+                ||!(txt_archive.Text.Length > 0) 
+                ||!(txt_image.Text.Length > 0))
+                return;
+
+            string str_folder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string str_destname = str_random();
+            string str_destpath = str_folder + "\\" + str_destname;
+            string str_tempimage = str_random() + str_image_ext;
+            string str_temparchive = str_random() + str_archive_ext;
+
+            str_destname += "_manipulated" + str_image_ext;
+
+            File.Copy(txt_image.Text, str_folder + "\\" + str_tempimage);
+            File.Copy(txt_archive.Text, str_folder + "\\" + str_temparchive);
+
+            var process = new System.Diagnostics.Process();
+            var startInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal,
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                UseShellExecute = false
+            };
+
+            process.StartInfo = startInfo;
+            process.Start();
+
+            process.StandardInput.WriteLine("cd " + str_folder);
+            process.StandardInput.WriteLine("copy /b " + str_tempimage + "+" + str_temparchive + " " + str_destname);
+            process.StandardInput.WriteLine("clear");
+            System.Threading.Thread.Sleep(100);
+            process.StandardInput.WriteLine("del " + str_tempimage);
+            process.StandardInput.WriteLine("del " + str_temparchive); // ghetto delete c0z we Livin in da h00d, not realy but im gay *cowboy emoji*
+            process.StandardInput.WriteLine("clear");
+            System.Threading.Thread.Sleep(100);
+            process.StandardInput.WriteLine("exit");
+
+            System.Threading.Thread.Sleep(20);
+
+
+        }
+    }
+}
